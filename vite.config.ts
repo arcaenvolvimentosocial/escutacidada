ae085728dd41db78f68ab91498c8b70796467fca
arcaenvolvimentosocial <arcaenvolvimentosocial@gmail.com>
2026-03-15 14:15:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/escutacidada/',
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1] || '';
+  const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
+  return {
+    plugins: [react()],
+    base: isGitHubPages && repo ? `/${repo}/` : '/',
+  };
+});
