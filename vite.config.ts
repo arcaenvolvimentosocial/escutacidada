ee2084276037fec95beac57db2350ba694138b2c
arcaenvolvimentosocial <arcaenvolvimentosocial@gmail.com>
2026-03-15 14:11:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dashboardp4/',
+  base: '/escutacidada/',
   plugins: [react()],
 })
