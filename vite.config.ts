d49847733c975316914b64ad828e107813854ca6
arcaenvolvimentosocial <arcaenvolvimentosocial@gmail.com>
2026-03-15 14:24:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')?.[1] || '';
-  const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
-  return {
-    plugins: [react()],
-    base: isGitHubPages && repo ? `/${repo}/` : '/',
-  };
-});
+export default defineConfig({
+  base: '/dashboardp4/',
+  plugins: [react()],
+})
